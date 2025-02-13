6d6b6f651d20bc9bd1e0f0d03f92086ec9c569a2
Igor <80629173+iddev13@users.noreply.github.com>
2025-02-13 12:05:35 +0200

    1.0.9

--- src/app/(market)/casual/casual-products.tsx
+++ src/app/(market)/casual/casual-products.tsx
@@ -1,6 +1,10 @@
+import { AppPagination } from '@/components/app-pagination';
+import { ShopCard } from '@/components/cards/shop-card';
+import { casualProductsData } from '@/constants';
+
 export const CasualProducts = () => {
 	return (
-		<div className="w-full">
+		<div>
 			<div className="flex items-end justify-between mb-4">
 				<h1 className="text-xl lg:text-3xl flex-auto">Casual</h1>
 				<div className="flex">
@@ -8,7 +12,21 @@ export const CasualProducts = () => {
 					<div>Sort by: Most Popular</div>
 				</div>
 			</div>
-			<div>products grid</div>
+			<div className="grid grid-cols-3 gap-x-5 gap-y-9 mb-4 pb-6 border-b">
+				{casualProductsData.map((card) => (
+					<ShopCard
+						key={card.id}
+						href={card.href}
+						imageUrl={card.imageUrl}
+						price={card.price}
+						oldPrice={card.oldPrice}
+						title={card.title}
+						rate={card.rate}
+						discount={card.discount}
+					/>
+				))}
+			</div>
+			<AppPagination className="px-4" />
 		</div>
 	);
 };
---
+++ src/components/app-pagination.tsx
@@ -0,0 +1,55 @@
+import {
+	Pagination,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from '@/components/ui/pagination';
+import { cn } from '@/lib/utils';
+
+type Props = {
+	className?: string;
+};
+
+export function AppPagination({ className }: Props) {
+	return (
+		<Pagination className={cn('w-full', className)}>
+			<PaginationContent className="w-full justify-between">
+				<PaginationItem>
+					<PaginationPrevious href="#" />
+				</PaginationItem>
+				<div className="flex items-center gap-2">
+					<PaginationItem>
+						<PaginationLink href="#">1</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#" isActive>
+							2
+						</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">3</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationEllipsis />
+					</PaginationItem>
+
+					<PaginationItem>
+						<PaginationLink href="#">8</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">9</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">10</PaginationLink>
+					</PaginationItem>
+				</div>
+				<PaginationItem>
+					<PaginationNext href="#" />
+				</PaginationItem>
+			</PaginationContent>
+		</Pagination>
+	);
+}
